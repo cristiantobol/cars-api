000a5742b307e4a082fc478cf8919741e7c690e8
Cristian <cristian.tobol@gmail.com>
2024-12-01 18:26:27 +0000

    Added endpoint for listing cars by make & model

--- controllers/cars.controller.ts
+++ controllers/cars.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import VehicleRepository from "../repositories/vehicle-repository"
+import VehicleRepository, { Vehicle } from "../repositories/vehicle-repository"
 
 const repository = new VehicleRepository()
 
@@ -8,6 +8,45 @@ export const getCars = (_: Request, res: Response) => {
     const vehicles = repository.getAll()
     res.status(200).json(vehicles)
   } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occurred while fetching vehicles")
+  }
+}
+
+export const getCarsByMake = (req: Request, res: Response) => {
+  try {
+    const { make } = req.params
+    const vehicles = repository.getAll()
+    const filteredVehicles = vehicles.filter((v: Vehicle) => v.make?.toLocaleLowerCase() === make?.toLocaleLowerCase())
+
+    if (filteredVehicles.length === 0) {
+      return res.status(404).send({ message: `No results found for ${make}` })
+    }
+
+    res.status(200).json(filteredVehicles)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occurred while fetching vehicles")
+  }
+}
+
+export const getCarsByModel = (req: Request, res: Response) => {
+  try {
+    const { model, make } = req.params
+    const vehicles = repository.getAll()
+    const filteredVehicles = vehicles.filter(
+      (v: Vehicle) =>
+        v.model?.toLocaleLowerCase() === model?.toLocaleLowerCase() &&
+        v.make?.toLocaleLowerCase() === make?.toLocaleLowerCase()
+    )
+
+    if (filteredVehicles.length === 0) {
+      return res.status(404).send({ message: `No results found for ${model}` })
+    }
+
+    res.status(200).json(filteredVehicles)
+  } catch (error) {
+    console.error(error)
     res.status(500).send("An error occurred while fetching vehicles")
   }
 }
--- routes/cars.routes.ts
+++ routes/cars.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
-import { getCars } from "../controllers/cars.controller"
+import { getCarsByMake, getCars, getCarsByModel } from "../controllers/cars.controller"
 
 const router = Router()
 
 router.get("/cars", getCars)
+router.get("/cars/:make", getCarsByMake)
+router.get("/cars/:make/:model", getCarsByModel)
 
 export default router
